f395d303136b6a17f118ed4c78aeb3e91ed164ee
Fathi Ayari <fathi3ayari333@gmail.com>
2021-10-26 11:05:54 +0200

    logic part of the app

--- app/src/main/java/com/fathi_ayari/tip_time/MainActivity.kt
+++ app/src/main/java/com/fathi_ayari/tip_time/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.fathi_ayari.tip_time
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fathi_ayari.tip_time.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding//later init
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root)//the whole layout main.xml
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
+
+
